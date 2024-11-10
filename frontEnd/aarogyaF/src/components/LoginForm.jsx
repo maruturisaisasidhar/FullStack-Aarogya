656f5e393a28ea1c92d07f9ff0a431e3c22541dc
Sai sasidhar Maruturi <132249602+maruturisaisasidhar@users.noreply.github.com>
2024-11-10 18:37:46 +0530

    Update LoginForm.jsx

--- frontEnd/aarogyaF/src/components/LoginForm.jsx
+++ frontEnd/aarogyaF/src/components/LoginForm.jsx
@@ -13,7 +13,7 @@ const LoginForm = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://fullstack-aarogya.onrender.com/api/auth/login",
         {
           username,
           password,
